c57420f9b1d6635564b28afedf763bf7394cabf7
Kostek <ksytnik73kwer@gmail.com>
2024-07-19 15:20:15 +0300

    feat: Add validation middleware for user registration

--- user-management-service/src/routes/userRoutes.js
+++ user-management-service/src/routes/userRoutes.js
@@ -1,14 +1,26 @@
-const express = require("express")
+const express = require('express');
+const { body } = require('express-validator');
 const {
   registerUser,
   loginUser,
   changeUsername,
 } = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
+const validateRequest = require('../middlewares/validationMiddleware');
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post(
+  '/register',
+  [
+    body('username').not().isEmpty().withMessage('Username is required'),
+    body('password')
+      .isLength({ min: 8 })
+      .withMessage('Password must be at least 8 characters long'),
+  ],
+  validateRequest,
+  registerUser
+);
 router.post('/login', loginUser);
 router.put('/change-username', protect, changeUsername);
 
