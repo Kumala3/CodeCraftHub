dbeff52e9cbb2be3d76807b99f7bbd04ece858e1
Kostek <ksytnik73kwer@gmail.com>
2024-07-19 14:59:21 +0300

    Update user routes and add errorMiddleware to app.js

--- user-management-service/src/app.js
+++ user-management-service/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { connectDB } = require('./config');
 const userRoutes = require('./routes/userRoutes');
+const errorHandler = require("./middlewares/errorMiddleware")
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+
+app.user(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
--- user-management-service/src/routes/userRoutes.js
+++ user-management-service/src/routes/userRoutes.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const express = require("express")
+const {
+  registerUser,
+  loginUser,
+  changeUsername,
+} = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.put('/change-username', protect, changeUsername);
 
 module.exports = router;
